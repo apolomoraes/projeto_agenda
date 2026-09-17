2fa13025b1e8ec8e8916bd3b860db9e732731fde
apolomoraes <vlogsapolo@gmail.com>
2026-09-17 20:31:38 -0300

    feat: add contact pages, controller, model and auth middleware

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -17,3 +17,13 @@ exports.csrfMiddleware = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
   next();
 }
+
+exports.loginRequired = (req, res, next) => {
+  if (!req.session.user) {
+    req.flash('errors', 'Usuário precisa estar logado');
+    req.session.save(() => res.redirect('/login/index'));
+    return;
+  }
+
+  next();
+}
