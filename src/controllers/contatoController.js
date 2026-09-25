c0af78ebe9502d860b784538b31d491dc46bacd5
apolomoraes <vlogsapolo@gmail.com>
2026-09-25 15:06:07 -0300

    feat: add contact deletion

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -51,3 +51,12 @@ exports.edit = async (req, res) => {
     return res.render('404');
   }
 };
+
+exports.delete = async (req, res) => {
+  if (!req.params.id) return res.render('404');
+  const contato = await Contato.delete(req.params.id);
+  if (!contato) return res.render('404');
+  req.flash('success', 'Contato removido com sucesso!');
+  req.session.save(() => res.redirect('/'));
+  return;
+};
